7e2d65862d7bb8bcd148679999bebec01aff5cf8
DeepmindsAI <deepmindsaitechnology@gmail.com>
2026-04-10 07:09:32 +0530

    Add files via upload

---
+++ AI Batch_April_2026/python/modulefun.py
@@ -0,0 +1,22 @@
+#print ("program for Module program calling")
+#a=10
+# add function
+def add(a,c,b):
+    sum = a+b+c
+    print ('in function sum : ' , sum )
+    name = 'abhi'
+    return name, sum
+
+# print function
+def pnt():
+    print('hello first function')
+    return 
+
+# Fibonacci numbers module
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a,b = b, a+b
+    print()
+
