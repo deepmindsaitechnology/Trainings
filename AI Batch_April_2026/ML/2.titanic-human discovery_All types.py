63efae624017d2471de78c352cf37c918fe79c6b
DeepmindsAI <deepmindsaitechnology@gmail.com>
2026-05-02 11:23:49 +0530

    Add files via upload

---
+++ AI Batch_April_2026/ML/2.titanic-human discovery_All types.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+#os.chdir("C:\\Users\\vesuraju\\OneDrive - DXC Production\\Venkat\\Personal\\Trainings\\ML\\Classes_Year 2020\\Codes_2020\\Datasets")
+os.chdir("C:\\Users\\ADMIN\\Desktop\\Trainings\\AI Batch_March\\3. ML\\data_source")
+
+titanic_train = pd.read_csv("train.csv")
+print(titanic_train.shape)
+
+print(titanic_train.info())
+
+#discover pattern: which class is majority?
+titanic_train.groupby('Survived').size()
+
+titanic_test = pd.read_csv("test.csv")
+print(titanic_test.shape)
+
+#os.chdir("C:\\Users\\vesuraju\\OneDrive - DXC Production\\Venkat\\Personal\\Trainings\\ML\\Classes_Year 2020\\Codes_2020\\Datasets\\Submissions")
+os.chdir("C:\\Users\\ADMIN\\Desktop\\Trainings\\AI Batch_March\\3. ML\\data_source\\submissions")
+
+#1.  Assume all are not survived as predeiction  
+titanic_test['Survived'] = 0
+titanic_test.to_csv("submission_01.csv", columns = ['PassengerId', 'Survived'], index=False)
+
+#2. discover pattern: which class is majority?
+titanic_train.groupby('Survived').size()
+titanic_train.groupby(['Sex','Survived']).size()
+
+print(titanic_test.shape)
+
+#guess based on patterns, here female has more chnaces to giving 100% as guess
+titanic_test['Survived'] = 0
+titanic_test.loc[titanic_test.Sex =='female', 'Survived'] = 1
+titanic_test.to_csv("submission_02.csv", columns = ['PassengerId', 'Survived'], index=False)
+
+# try to find more patterns with more no of features.
+
+#discover pattern: which class is majority?
+titanic_train.groupby('Survived').size()
+titanic_train.groupby(['Sex','Survived']).size()
+titanic_train.groupby(['Sex','Pclass','Survived']).size()
+titanic_train.groupby(['Sex','Embarked','Survived']).size()
+
+titanic_test.loc[(titanic_train.Sex=='female') & (titanic_train.Pclass==1), 'Survived'] = 1 
+titanic_test.to_csv("submission_03.csv", columns = ['PassengerId', 'Survived'], index=False)                
